8d737bc6a428eb18795bf1cb6a094534c0b290d0
Jose Ramon Aleman <jraleman@bendiburg.com>
2017-09-26 06:32:44 -0700

    Creating a run_norme script, and adding headers to tools' script files

--- files/tools/clean_directories.sh
+++ files/tools/clean_directories.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    clean_directories.sh                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/09/20 16:25:39 by jaleman           #+#    #+#              #
+#    Updated: 2017/09/20 16:25:40 by jaleman          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 find ../ -name '._*' -type f -delete
 find ../ -name '.DS_Store' -type f -delete
 find ../ -name '.Trashes' -type f -delete
