3846704d3e119ee48de7e5b29d76f078e6ffb2ce
jraleman <jraleman@bendiburg.com>
2019-02-26 07:21:42 -0800

    Create script to install hombrew

---
+++ files/install_hombrew.sh
@@ -0,0 +1,6 @@
+mkdir $HOME/.brew && curl -fsSL https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.brew
+mkdir -p /tmp/.$(whoami)-brew-locks
+mkdir -p $HOME/.brew/var/homebrew
+ln -s /tmp/.$(whoami)-brew-locks $HOME/.brew/var/homebrew/locks
+export PATH="$HOME/.brew/bin:$PATH"
+brew update && brew upgrade
